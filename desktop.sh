da1c5c7b91c9883f36e2db13284844e568da8c67
Jose Chirivella <jrchg1403@gmail.com>
2015-09-02 19:00:28 -0400

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-echo This functionality of this script it's to install Ruby on Rails and NGINX with Passenger.
+echo This functionality of this script it's to install Ruby on Rails 
 sudo apt-get update
 sudo apt-get install build-essential  libssl-dev libcurl4-openssl-dev openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison nodejs
 curl -L get.rvm.io | bash -s stable
